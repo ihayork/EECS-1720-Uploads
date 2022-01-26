55a91365ce0123b84585bc8ee2a1608240513cca
ihayork <90470940+ihayork@users.noreply.github.com>
2022-01-26 13:54:16 -0500

    updated with new features

--- LabProject1/Code/content.js
+++ LabProject1/Code/content.js
@@ -6,8 +6,34 @@ chrome.runtime.onMessage.addListener(function(request) {
             hyperlinks[i].style.color = request.pack2;
         }
     }
-    
-    //const re = new RegExp('a', "gi");
-    //const matches = document.documentElement.innerHTML.match(re);
-    //alert("the string a was found " + matches.length + " times on this page.");
+    else if(request.pack1 == "textColor"){
+        var paragraphs = document.getElementsByTagName("p");
+        for(i = 0; i < paragraphs.length; i++){
+            paragraphs[i].style.color = request.pack2;
+        }
+    }
+    else if(request.pack1 == "headingColor"){
+        var h1 = document.getElementsByTagName("h1");
+        var h2 = document.getElementsByTagName("h1");
+        var h3 = document.getElementsByTagName("h1");
+        var h4 = document.getElementsByTagName("h1");
+        for(i = 0; i < h1.length; i++){
+            h1[i].style.color = request.pack2;
+        }
+        for(i = 0; i < h2.length; i++){
+            h2[i].style.color = request.pack2;
+        }
+        for(i = 0; i < h3.length; i++){
+            h3[i].style.color = request.pack2;
+        }
+        for(i = 0; i < h4.length; i++){
+            h4[i].style.color = request.pack2;
+        }
+    }
+    else if(request.pack1 == "textString"){
+        const find = new RegExp(request.pack2, "gi");
+        var pageHTML = document.documentElement.innerHTML;
+        const matches = (pageHTML.match(find) || []);
+        alert("The string " + request.pack2 + " was found " + matches.length + " times in this HTML document.");
+    }
 })
--- LabProject1/Code/popup.js
+++ LabProject1/Code/popup.js
@@ -1,5 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('hyperlinkColor').addEventListener('click', hyperlinkColor, false);
+    document.getElementById('textColor').addEventListener('click', textColor, false);
+    document.getElementById('headingColor').addEventListener('click', headingColor, false);
+    document.getElementById('textString').addEventListener('click', textString, false);
     //more buttons here
 
     function hyperlinkColor() {
@@ -10,6 +13,39 @@ document.addEventListener('DOMContentLoaded', function() {
             chrome.tabs.sendMessage(currTab.id, {
                 pack1: "hyperlinkColor",
                 pack2: selectedColor
+            });
+        });
+    }
+    function textColor() {
+        var selectedColor = document.getElementById('textColorSelect').value;
+        chrome.tabs.query({currentWindow: true, active: true}, 
+        function(tabs) {
+            var currTab = tabs[0];
+            chrome.tabs.sendMessage(currTab.id, {
+                pack1: "textColor",
+                pack2: selectedColor
+            });
+        });
+    }
+    function headingColor() {
+        var selectedColor = document.getElementById('headingColorSelect').value;
+        chrome.tabs.query({currentWindow: true, active: true}, 
+        function(tabs) {
+            var currTab = tabs[0];
+            chrome.tabs.sendMessage(currTab.id, {
+                pack1: "headingColor",
+                pack2: selectedColor
+            });
+        });
+    }
+    function textString() {
+        var selectedText = document.getElementById('textStringSelect').value;
+        chrome.tabs.query({currentWindow: true, active: true}, 
+        function(tabs) {
+            var currTab = tabs[0];
+            chrome.tabs.sendMessage(currTab.id, {
+                pack1: "textString",
+                pack2: selectedText
             });
         });
     }
